6ae14e0bc3649fd67dda91c22adacd6a32491c41
Flavio-Tolosana <flavio.tolosana@gmail.com>
2024-03-11 15:10:02 +0000

    Inicio proyecto con separacion deploy - main

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import bjLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [avatarData, setAvatarData] = useState(null);
 
   const handleClick = async () => {
+    setCount((count) => count + 1)
     const response = await fetch('https://backend-uf65.onrender.com/api/avatar/avatarById/65ea25df0b5bd47a75ff5da8', {
         method: 'GET',
     })
     const json = await response.json()
     if (response.ok) {
       console.log(json)
+      setAvatarData(json.avatar)
     }else {
       console.log("Nada, ERROR")
     }
@@ -21,28 +23,25 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/UNIZAR-30226-2024-07/Frontend_Web/" target="_blank">
+          <img src={bjLogo} className="logo" alt="Black Jack Master logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Black Jack Master</h1>
+      <button className='button' onClick={handleClick}>Obtener un Avatar</button>
+
+      {avatarData && (
+        <div>
+          <p>Avatar ID: {avatarData._id}</p>
+          <p>Avatar Image: {avatarData.image}</p>
+          <p>Avatar Price: {avatarData.price}</p>
+          <p>Created At: {avatarData.createdAt}</p>
+          <p>Updated At: {avatarData.updatedAt}</p>
+        </div>
+      )}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       <button className='button'>Numero veces pulsado boton 'Obtener Avatar': {count}</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <button className='button' onClick={handleClick}>ObtenerAvatar</button>
-
     </>
   )
 }
