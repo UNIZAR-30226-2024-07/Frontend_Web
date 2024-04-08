305ab2a6d670149e90bfdd132ea94feff45b7292
sergioes55 <96990703+sergioes55@users.noreply.github.com>
2024-04-08 17:41:57 +0200

    Arreglar repo Sergio

--- src/Router/RouterPrincipal.jsx
+++ src/Router/RouterPrincipal.jsx
@@ -2,6 +2,9 @@ import {Route, BrowserRouter, Routes} from "react-router-dom"
 import { AuthProvider } from "../Context/AuthContext"
 import { ProtectedRoute } from "./ProtectedRoute"
 import { PageHome } from "../Pages/PageHome"
+import { PageFriendFind } from "../Pages/PageFriendFind"
+import { PageFriendList } from "../Pages/PageFriendList"
+import { PageFriendRequest } from "../Pages/PageFriendRequest"
 import { PageRegister } from "../Pages/PageRegister"
 import { PageLogin } from "../Pages/PageLogin"
 import constants from '../constants'
@@ -20,10 +23,15 @@ const RouterPrincipal = () => {
                 <Routes>
                     <Route path={constants.root} element={<PageHome/>} ></Route>
                     <Route path={constants.root + 'CrearCuentaAdmin'} element={<CrearCuentaAdmin/>}></Route>
-                    <Route path={constants.root + 'CrearSala'} element={<CrearSala/>}></Route>                 
+                    <Route path={constants.root + 'CrearSala'} element={<CrearSala/>}></Route>
+                    <Route path={constants.root + 'PageLogin'} element={<PageLogin/>}></Route>  
+                    <Route path={constants.root + 'PageRegister'} element={<PageRegister/>}></Route>                   
 
                     <Route element={<ProtectedRoute />}>
                         
+                        <Route path={constants.root + 'PageFriendFind'} element={<PageFriendFind/>}></Route>
+                        <Route path={constants.root + 'PageFriendList'} element={<PageFriendList/>}></Route>
+                        <Route path={constants.root + 'PageFriendRequest'} element={<PageFriendRequest/>}></Route>
                         <Route path={constants.root + 'PageDashboard'} element={<PageDashboard/>}></Route>
                         <Route path={constants.root + 'ejemploSubidaFoto'} element={<EjemploSubidaFoto/>}></Route>
                         <Route path={constants.root + 'pruebaMatch'} element={<PruebaMatch/>}></Route>
