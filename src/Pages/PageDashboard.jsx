8e0ae48b0d76bd0f1221879e8128368e1bb6ab32
Sergio Martinez <martinezlahoz2001@gmail.com>
2024-04-10 14:48:29 +0000

    por si acaso

--- src/Pages/PageDashboard.jsx
+++ src/Pages/PageDashboard.jsx
@@ -2,20 +2,29 @@ import { MyNav } from "../Components/MyNav";
 import { MyAvatar } from "../Components/MyAvatar";
 import { MyButton } from "../Components/MyButton";
 import "./PageDashboard.css"
+import { useNavigate } from "react-router-dom";
 
 export function PageDashboard() {
-    return (
-      <>
-        <MyNav isLoggedIn={false} isDashboard={true}/>
-        <div className="avatar-dashboard">
-            <MyAvatar>
-            </MyAvatar>
-        </div>
-        <div className="option-dashboard">
-            <MyButton color="midnightblue" size="xxl" variant="bordered">Partida Publica</MyButton>
-            <MyButton color="midnightblue" size="xxl" variant="bordered">Partida Privada</MyButton>
-            <MyButton color="midnightblue" size="xxl" variant="bordered">Torneo</MyButton>
-        </div>
-      </>
+  const navigate = useNavigate();
+
+  const ajustes = () => {
+    navigate('/Frontend_Web/AjustesUser');
+  }
+  
+  return (
+    <>
+      <MyNav isLoggedIn={false} isDashboard={true}/>
+      <div className="avatar-dashboard">
+          <MyAvatar>
+          </MyAvatar>
+      </div>
+      <div className="option-dashboard">
+          <MyButton color="midnightblue" size="xxl" variant="bordered">Partida Publica</MyButton>
+          <MyButton color="midnightblue" size="xxl" variant="bordered">Partida Privada</MyButton>
+          <MyButton color="midnightblue" size="xxl" variant="bordered">Torneo</MyButton>
+          <MyButton color="midnightblue" size="xxl" variant="bordered" onClick={ajustes}>Ajustes</MyButton>
+
+      </div>
+    </>
   );
 }
