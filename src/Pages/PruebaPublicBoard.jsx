0505d5c52a5056f805bb31969f46a2d590d3d8e5
Flavio-Tolosana <flavio.tolosana@gmail.com>
2024-04-25 10:36:47 +0000

    Prueba public actualizado. Inicializa bien cartas. Falta comprobar funcion split

--- src/Pages/PruebaPublicBoard.jsx
+++ src/Pages/PruebaPublicBoard.jsx
@@ -23,7 +23,8 @@ const PruebaPublicBoard = () => {
         total: 0, // Suma de la puntuación de las cartas
         defeat: false, // Si defeat = true, el jugador ha perdido esta mano
         blackJack: false, // blackJack = true cuando se tienen 21 puntos
-        active: false // TODO: 
+        active: false, // TODO: 
+        stick: false   // Indica si se ha plantado o no el jugador con stick
     }
     // Información de un jugador
     const objPlayer = {
@@ -38,8 +39,9 @@ const PruebaPublicBoard = () => {
 
     // Información todos los jugadores
     // El jugador players[0] es el user
-    const [players, setPlayers] = useState([]);   
     const [bank, setBank] = useState(objBank)   // Información de la banca
+    const [player, setPlayer] = useState(objPlayer);   
+    const [restPlayers, setRestPlayers] = useState([]);   // Información resto jugadores
 
     const [tituloVisible, setTituloVisible] = useState(false) // Variable "tonta" para ver cuando se ha conseguido una partida
     const [partidasPublicas, setPartidasPublicas] = useState([]) // Lista de partidas públicas
@@ -62,12 +64,12 @@ const PruebaPublicBoard = () => {
     ////////////////////////////////////////////////////////////////////////////
 
     // numHand: 0 si es la primera mano / 1 si es la segunda mano
-    const drawCard = async (event, numHand, players, setPlayers) => {
+    const drawCard = async (event, numHand, player, setPlayer) => {
         event.preventDefault()
         try {
             const response = await axios.put('/publicBoard/drawCard', {
                 boardId: boardId,
-                cardsOnTable: players[indexUser].hands[numHand].cards
+                cardsOnTable: player.hands[numHand].cards
             })
             if (response.status !== 200) {
                 console.log("Fallo: ", response);
@@ -75,102 +77,105 @@ const PruebaPublicBoard = () => {
             }
 
             // console.log('DrawCard: ', response.data);
-            const updatedPlayers = [...players]
-            updatedPlayers[indexUser].hands[numHand].cards = response.data.cardsOnTable
-            updatedPlayers[indexUser].hands[numHand].total = response.data.totalCards
-            updatedPlayers[indexUser].hands[numHand].defeat = response.data.defeat
-            updatedPlayers[indexUser].hands[numHand].blackJack = response.data.blackJack    
-            updatedPlayers[indexUser].hands[numHand].active = true
-            setPlayers(updatedPlayers)
+            const updatedPlayer = {...player}
+            updatedPlayer.hands[numHand].cards = response.data.cardsOnTable
+            updatedPlayer.hands[numHand].total = response.data.totalCards
+            updatedPlayer.hands[numHand].defeat = response.data.defeat
+            updatedPlayer.hands[numHand].blackJack = response.data.blackJack    
+            updatedPlayer.hands[numHand].active = true
+            setPlayer(updatedPlayer)
 
         } catch (error) {
             console.error("Error:", error);
         }
     }
 
-    const split = async (event, players, setPlayers) => {
+    const split = async (event, player, setPlayer) => {
         event.preventDefault()
         try {
             const response = await axios.put('/publicBoard/split', {
                 boardId: boardId,
-                cardsOnTable: players[indexUser].hands[hand0].cards
+                cardsOnTable: player.hands[hand0].cards
             })
             if (response.status !== 200) {
                 console.log("Fallo: ", response);
                 throw new Error('Error', response);
             }
             // console.log('Avatares:', response);
-            const updatedPlayers = [...players]
+            const updatedPlayer = {...player}
 
-            updatedPlayers[indexUser].hands[hand0].cards = response.data.cardsOnTableFirst
-            updatedPlayers[indexUser].hands[hand0].total = response.data.totalCardsFirst
-            updatedPlayers[indexUser].hands[hand0].defeat = response.data.defeatFirst
-            updatedPlayers[indexUser].hands[hand0].blackJack = response.data.blackJackFirst  
-            updatedPlayers[indexUser].hands[hand0].active = true
+            updatedPlayer.hands[hand0].cards = response.data.cardsOnTableFirst
+            updatedPlayer.hands[hand0].total = response.data.totalCardsFirst
+            updatedPlayer.hands[hand0].defeat = response.data.defeatFirst
+            updatedPlayer.hands[hand0].blackJack = response.data.blackJackFirst  
+            updatedPlayer.hands[hand0].active = true
 
-            updatedPlayers[indexUser].hands[hand1].cards = response.data.cardsOnTableSecond
-            updatedPlayers[indexUser].hands[hand1].total = response.data.totalCardsSecond
-            updatedPlayers[indexUser].hands[hand1].defeat = response.data.defeatSecond
-            updatedPlayers[indexUser].hands[hand1].blackJack = response.data.blackJackSecond  
-            updatedPlayers[indexUser].hands[hand1].active = true
+            updatedPlayer.hands[hand1].cards = response.data.cardsOnTableSecond
+            updatedPlayer.hands[hand1].total = response.data.totalCardsSecond
+            updatedPlayer.hands[hand1].defeat = response.data.defeatSecond
+            updatedPlayer.hands[hand1].blackJack = response.data.blackJackSecond  
+            updatedPlayer.hands[hand1].active = true
 
-            setPlayers(updatedPlayers)
+            setPlayer(updatedPlayer)
 
         } catch (error) {
             console.error("Error:", error);
         }
     }
 
-    const double = async (event, numHand, players, setPlayers) => {
+    const double = async (event, numHand, player, setPlayer) => {
         event.preventDefault()
         try {
             const response = await axios.put('/publicBoard/double', {
                 boardId: boardId,
-                cardsOnTable: players[0].hand[numHand].cards
+                cardsOnTable: player.hands[numHand].cards
             })
             if (response.status !== 200) {
                 console.log("Fallo: ", response);
                 throw new Error('Error', response);
             }
             // console.log('Avatares:', response);
-            const updatedPlayers = [...players]
-            updatedPlayers[indexUser].hands[numHand].cards = response.data.cardsOnTable
-            updatedPlayers[indexUser].hands[numHand].total = response.data.totalCards
-            updatedPlayers[indexUser].hands[numHand].defeat = response.data.defeat
-            updatedPlayers[indexUser].hands[numHand].blackJack = response.data.blackJack    
-            updatedPlayers[indexUser].hands[numHand].active = true
-            setPlayers(updatedPlayers)
+            const updatedPlayer = {...player}
+            updatedPlayer.hands[numHand].cards = response.data.cardsOnTable
+            updatedPlayer.hands[numHand].total = response.data.totalCards
+            updatedPlayer.hands[numHand].defeat = response.data.defeat
+            updatedPlayer.hands[numHand].blackJack = response.data.blackJack    
+            updatedPlayer.hands[numHand].active = true
+            setPlayer(updatedPlayer)
         } catch (error) {
             console.error("Error:", error);
         }
     }
 
-    const stick = async (event, numHand, players) => {
+    const stick = async (event, numHand, player) => {
         event.preventDefault()
         try {
             const response = await axios.put('/publicBoard/stick', {
                 boardId: boardId,
-                cardsOnTable: players[indexUser].hands[numHand].cards
+                cardsOnTable: player.hands[numHand].cards
             })
             if (response.status !== 200) {
                 console.log("Fallo: ", response);
                 throw new Error('Error', response);
             }
             console.log('RESULTADO GUARDADO CORRECTAMENTE');
+            const updatedPlayer = {...player}
+            updatedPlayer.hands[numHand].stick = true
+            setPlayer(updatedPlayer)
         } catch (error) {
             console.error("Error:", error);
         }
     }
 
-    useEffect(() => {
-        const newPlayers = []
-        for (let i = 0; i < numPlayers; i++) {
-            const playerCopy = {...objPlayer}
-            newPlayers.push(playerCopy)
-        }
-        setPlayers(newPlayers)
-        console.log("newPlayers", newPlayers)
-    }, [])
+    // useEffect(() => {
+    //     const newPlayers = []
+    //     for (let i = 0; i < numPlayers; i++) {
+    //         const playerCopy = {...objPlayer}
+    //         newPlayers.push(playerCopy)
+    //     }
+    //     setPlayers(newPlayers)
+    //     console.log("newPlayers", newPlayers)
+    // }, [])
 
     useEffect(() => {
         getPartidasPublicas()
@@ -194,52 +199,85 @@ const PruebaPublicBoard = () => {
             
             // Guardar banca
             // Información de la banca en el último componente initCards
-            const updatedBank = {...bank}
-            updatedBank.hand.cards = initCards[initCards.length - 1].cards
-            updatedBank.hand.total = initCards[initCards.length - 1].totalCards
-            updatedBank.hand.blackJack = initCards[initCards.length - 1].blackJack
-            updatedBank.hand.active = true
-            setBank(updatedBank)
-            
-            // const updatedPlayers = [...players]
-            const updatedPlayers = []
-            
-            console.log("Inicial:", updatedPlayers)////////////////////////////////////////////////////////////////
-            
+            const bankObj = {
+                playerId: 'Bank',
+                hand: {
+                        cards: initCards[initCards.length - 1].cards, // Vector que contiene las cartas de la jugada
+                        total: initCards[initCards.length - 1].totalCards, // Suma de la puntuación de las cartas
+                        defeat: false, // Si defeat = true, el jugador ha perdido esta mano
+                        blackJack: initCards[initCards.length - 1].blackJack, // blackJack = true cuando se tienen 21 puntos
+                        active: true, // TODO: 
+                        stick: false   // Indica si se ha plantado o no el jugador con stick
+                    }
+            }
+            setBank(bankObj)
+                        
             // Guardar primeras dos cartas usuario
             const infoPlayer = initCards.find(infoPlayer => infoPlayer.userId === user._id);
-            let playerObj
-            playerObj.playerId = infoPlayer.userId
-            playerObj.hands[hand0].cards = infoPlayer.cards
-            playerObj.hands[hand0].total = infoPlayer.totalCards
-            playerObj.hands[hand0].blackJack = infoPlayer.blackJack
-            updatedPlayers.push(playerObj)
-
-
-            console.log("Antes asignar:", updatedPlayers)////////////////////////////////////////////////////////////////
+            
+            // Información de un jugador
+            const playerObj = {
+                playerId: infoPlayer.userId,
+                hands: [
+                    {
+                        cards: infoPlayer.cards, // Vector que contiene las cartas de la jugada
+                        total: infoPlayer.totalCards, // Suma de la puntuación de las cartas
+                        defeat: false, // Si defeat = true, el jugador ha perdido esta mano
+                        blackJack: infoPlayer.blackJack, // blackJack = true cuando se tienen 21 puntos
+                        active: true, // TODO: 
+                        stick: false   // Indica si se ha plantado o no el jugador con stick
+                    },{
+                        cards: [], // Vector que contiene las cartas de la jugada
+                        total: 0, // Suma de la puntuación de las cartas
+                        defeat: false, // Si defeat = true, el jugador ha perdido esta mano
+                        blackJack: false, // blackJack = true cuando se tienen 21 puntos
+                        active: false, // TODO: 
+                        stick: false   // Indica si se ha plantado o no el jugador con stick
+                    }
+                ]
+            }
+            setPlayer(playerObj)
 
             // Asignar resto de jugadores
+            const restPlayersArray = []
             // Recorrer initBoards menos última componente que es la Banca
             // let indexPlayers = 1
             for (let i = 0; i < initCards.length - 1; i++) {
                 // Si no es el usuario
                 if (initCards[i].userId !== user._id) {
-                    let otherPlayerObj
-                    otherPlayerObj.playerId = initCards[i].userId
-                    otherPlayerObj.hands[hand0].cards = initCards[i].cards
-                    otherPlayerObj.hands[hand0].total = initCards[i].totalCards
-                    otherPlayerObj.hands[hand0].blackJack = initCards[i].blackJack
-                    otherPlayerObj.hands[hand0].active = true
-                    updatedPlayers.push(otherPlayerObj)
-                    // indexPlayers = indexPlayers + 1
+                    const otherPlayerObj = {
+                        playerId: initCards[i].userId,
+                        hands: [
+                            {
+                                cards: initCards[i].cards, // Vector que contiene las cartas de la jugada
+                                total: initCards[i].totalCards, // Suma de la puntuación de las cartas
+                                defeat: false, // Si defeat = true, el jugador ha perdido esta mano
+                                blackJack: initCards[i].blackJack, // blackJack = true cuando se tienen 21 puntos
+                                active: true, // TODO: 
+                                stick: false   // Indica si se ha plantado o no el jugador con stick
+                            },{
+                                cards: [], // Vector que contiene las cartas de la jugada
+                                total: 0, // Suma de la puntuación de las cartas
+                                defeat: false, // Si defeat = true, el jugador ha perdido esta mano
+                                blackJack: false, // blackJack = true cuando se tienen 21 puntos
+                                active: false, // TODO: 
+                                stick: false   // Indica si se ha plantado o no el jugador con stick 
+                            }
+                        ]
+                    }
+                    restPlayersArray.push(otherPlayerObj)
                 }
             }
-
-            console.log("Después asignar:", updatedPlayers)////////////////////////////////////////////////////////////////
-
-            setPlayers(updatedPlayers)
+            setRestPlayers(restPlayersArray)
         })
-    }, [tituloVisible, user, bank, players]) // Se ejecuta solo una vez cuando el componente se monta
+    }, [tituloVisible, user, bank]) // Se ejecuta solo una vez cuando el componente se monta
+
+    // Imprimir información de los jugadores
+    const imprimir = () => {
+        console.log('Bank: ', bank)
+        console.log('Player: ', player)
+        console.log('Rest Players: ', restPlayers)
+    }
 
     return (
         <div>
@@ -287,20 +325,25 @@ const PruebaPublicBoard = () => {
                 <p>Jugador:</p>
                 {[hand0, hand1].map(numHand => (
                     <div key={numHand + "-" + indexUser} style={{ backgroundColor: 'brown' }}>
-                        { players[indexUser] && players[indexUser].hands[numHand].active && (
+                        { player && player.hands[numHand].active && (
                             <div>
-                                <p>Mano {numHand}</p>
-                                {!players[indexUser].hands[numHand].defeat && 
-                                 !players[indexUser].hands[numHand].blackJack && (
+                                <p>Mano {numHand} / Total: {player.hands[numHand].total}</p>
+                                {!player.hands[numHand].defeat && 
+                                 !player.hands[numHand].blackJack &&
+                                 !player.hands[numHand].stick ? (
                                     <div>
-                                        <button onClick={(e) => drawCard(e, numHand, players, setPlayers)}>DrawCard</button>
-                                        <button onClick={(e) => split(e, players, setPlayers)}>Split</button>
-                                        <button onClick={(e) => double(e, numHand, players, setPlayers)}>Double</button>
-                                        <button onClick={(e) => stick(e, numHand, players)}>Stick</button>
+                                        <button onClick={(e) => drawCard(e, numHand, player, setPlayer)}>DrawCard</button>
+                                        <button onClick={(e) => split(e, player, setPlayer)}>Split</button>
+                                        <button onClick={(e) => double(e, numHand, player, setPlayer)}>Double</button>
+                                        <button onClick={(e) => stick(e, numHand, player)}>Stick</button>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <p>No puede realizar más jugadas. Se ha plantado.</p>
                                     </div>
                                 )}
                                 {/* Renderizar las cartas */}
-                                {players[indexUser].hands[numHand].cards.map((card, cardIndex) => (
+                                {player.hands[numHand].cards.map((card, cardIndex) => (
                                     <p key={cardIndex}>{card.value + '-' + card.suit}</p>    // Cambiar por imagen
                                 ))}
                             </div>
@@ -314,17 +357,17 @@ const PruebaPublicBoard = () => {
             {/* Mostrar resto JUGADORES */}
             <div style={{ backgroundColor: 'blue' }}>
                 {/* Iterar sobre los jugadores */}
+                <p>Resto jugadores:</p>
                 {(() => {
                     const jsxArray = [];
-                    // for (let index = indexUser + 1; index < numPlayers; index++) {
-                    for (let index = indexUser + 1; index < players.length; index++) {
+                    for (let index = 0; index < restPlayers.length; index++) {
                         const playerHands = [];
                         [hand0, hand1].forEach(numHand => {
-                            if ( players[index] && players[index].hands[numHand].active) {
+                            if ( restPlayers[index] && restPlayers[index].hands[numHand].active) {
                                 const handJSX = (
                                     <div key={numHand + "-" + index} style={{ backgroundColor: 'green' }}>
                                         {/* Renderizar las cartas */}
-                                        {players[index].hands[numHand].cards.map((card, cardIndex) => (
+                                        {restPlayers[index].hands[numHand].cards.map((card, cardIndex) => (
                                             <p key={cardIndex}>{card.value + '-' + card.suit}</p> // Cambiar por imagen
                                         ))}
                                     </div>
@@ -337,6 +380,10 @@ const PruebaPublicBoard = () => {
                     return jsxArray;
                 })()}
             </div>
+
+            <button type="submit" className="imprimir" onClick={imprimir}>
+                Imprimir info jugadores
+            </button>
         </div>
     )
 }
